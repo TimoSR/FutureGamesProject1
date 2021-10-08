850b1f4d96456b4fce781e690c7e7601b5a8224c
daniel <danielwik@live.se>
2021-10-08 09:23:55 +0200

    adding the correct movement

--- My project/Assets/Daniel/Dan_scripts/CharacterController.cs
+++ My project/Assets/Daniel/Dan_scripts/CharacterController.cs
@@ -5,28 +5,29 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     public float speed = 10;
-    public float gravity = 20;
-    public bool isOnGround = true;
-
-
+    //public float gravity = 20;
 
     Rigidbody2D body;
+    BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
+    private Animator anim;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
-
+        boxCollider = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-     //Horizontal movement
+        //Horizontal movement
         float HorizontalInput = Input.GetAxis("Horizontal");
 
         body.velocity = new Vector3(HorizontalInput * speed, body.velocity.y);
-        Physics.gravity = new Vector3(0, -gravity, 0);
 
-        // flips player towards the running direction
+        //flips player towards the running direction
         if (HorizontalInput > 0.01f)
         {
             transform.localScale = Vector3.one;
@@ -35,24 +36,61 @@ public class CharacterController : MonoBehaviour
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
+
         //jump
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
         {
             Jump();
         }
+
+        // slide on walls thing
+        if (onWall())
+        {
+
+            anim.SetTrigger("IsOnWall");
+            body.velocity = Vector2.zero;
+            body.gravityScale = 70;
+        }
+        else
+        {
+            body.gravityScale = 3;
+        }
+
+
+        //Animator states
+        anim.SetBool("Run", HorizontalInput != 0);
+        anim.SetBool("Grounded", isGrounded());
+        anim.SetBool("IsOnWall", onWall());
     }
+
+    // void wallslide()
+    // {
+    //     body.velocity = new vector3(body.velocity.x, body.gravityscale *=2, body.velocity.y);
+    //     anim.settrigger("isonwall");
+    //     onwall();
+    //}
+
 
     void Jump()
     {
         body.velocity = new Vector3(body.velocity.x, speed, body.velocity.y);
-        isOnGround = false;
+        anim.SetTrigger("Jump");
+        isGrounded();
     }
 
-    private void OnCollisionEnter(Collision collision)
+
+    // Checks if the player is gounded using boxCast and physics2D
+    // Not sure if the BoxCollider2D and RigidBody2D will break the game
+    private bool isGrounded()
     {
-        if(collision.gameObject.tag == "Ground")
-        {
-            isOnGround = true;
-        }
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+
+    private bool onWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
     }
 }
