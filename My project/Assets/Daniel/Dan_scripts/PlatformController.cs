590d5c99bbc301850125fa44d37b53d8938457ef
Timothy Rasmussen <Timothy.s.rasmussen@gmail.com>
2021-10-13 12:18:49 +0200

    Player Moving with Platform

--- My project/Assets/Daniel/Dan_scripts/PlatformController.cs
+++ My project/Assets/Daniel/Dan_scripts/PlatformController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -93,4 +94,20 @@ public class PlatformController : MonoBehaviour
         }
     }
 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.parent = null;
+        }
+    }
 }
