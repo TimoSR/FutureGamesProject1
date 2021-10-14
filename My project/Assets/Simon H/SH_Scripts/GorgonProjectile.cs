84e8254410bdbefd1bd425dcd40c1b9bc6d95346
Timothy Rasmussen <Timothy.s.rasmussen@gmail.com>
2021-10-14 16:28:16 +0200

    Map Update

--- My project/Assets/Simon H/SH_Scripts/GorgonProjectile.cs
+++ My project/Assets/Simon H/SH_Scripts/GorgonProjectile.cs
@@ -21,12 +21,12 @@ public class GorgonProjectile : MonoBehaviour
             transform.localEulerAngles =  Vector3.zero;
         }
     }
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            Destroy(other.gameObject); // Destroying the player for now but put the Death() here instead I guess
-        }
-        Destroy(this.gameObject);
-    }
+    // void OnCollisionEnter2D(Collision2D other)
+    // {
+    //     if (other.gameObject.tag == "Player")
+    //     {
+    //         Destroy(other.gameObject); // Destroying the player for now but put the Death() here instead I guess
+    //     }
+    //     Destroy(this.gameObject);
+    // }
 }
